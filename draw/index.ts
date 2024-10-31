0c4283d35fb998c20e6c2eb4969348c14a0b3298
Yaroslav <usern1861@gmail.com>
2024-10-31 22:14:49 +0200

    Update project files

--- draw/index.ts
+++ draw/index.ts
@@ -2,12 +2,87 @@ const openSelectColor = document.getElementById("openSelectColor") as HTMLSpanEl
 const draw = document.getElementById("draw") as HTMLDivElement
 const selectColor = document.getElementById("selectColor") as HTMLDivElement
 
-openSelectColor.addEventListener("click", function(): void{
-    if(draw.classList.value){
+const changeSizeField = document.getElementById("changeSizeField") as HTMLInputElement
+const numColumns = document.getElementById("numColumns") as HTMLInputElement
+const numLines = document.getElementById("numLines") as HTMLInputElement
+const fieldDraw = document.getElementById("fieldDraw") as HTMLDivElement
+
+let colorField: string = "#123346"
+let color: string = "red"
+
+openSelectColor.addEventListener("click", function (): void {
+    if (draw.classList.value) {
         draw.classList.remove("active")
-        selectColor.classList.remove("active")
-    }else{
+        selectColor.classList.remove("ac tive")
+    } else {
         draw.classList.add("active")
         selectColor.classList.add("active")
     }
 })
+
+changeSizeField.addEventListener("click", function (): void {
+    if (numColumns.value && numLines.value) {
+        fieldDraw.innerHTML = ""
+        const table = document.createElement("table") as HTMLTableElement
+        let isDrawing: boolean = false 
+        table.addEventListener("mouseleave", () => {
+            isDrawing = false;
+        });
+        for (let i = 0; i < parseInt(numColumns.value); i++){
+            const tr = document.createElement("tr") as HTMLTableRowElement
+            for(let j = 0; j < parseInt(numLines.value); j++){
+                const td = document.createElement("td") as HTMLTableDataCellElement
+                td.style.border = "1px solid black";
+                td.addEventListener("mousedown", function(): void{
+                    isDrawing = true
+                    td.style.background = color
+                })
+                td.addEventListener("mousemove", function(): void{
+                    if(isDrawing) td.style.background = color
+                })
+                td.addEventListener("mouseup", function(): void{
+                    isDrawing = false
+                })
+                tr.append(td)
+            }
+            table.append(tr)
+        }
+        fieldDraw.append(table)
+    }
+})
+
+const changeColors = document.querySelectorAll(".changeColors") as NodeListOf<HTMLInputElement>
+
+changeColors.forEach((input) => {
+    if (input.type === "button") {
+        input.addEventListener("click", function(): void {
+            color = this.value
+        });
+    } else {
+        input.addEventListener("change", function(): void {
+            color = this.value
+        });
+    }
+});
+
+const changeColorsField = document.querySelectorAll(".changeColorsField") as NodeListOf<HTMLInputElement>
+const eraser = document.getElementById("eraser") as HTMLInputElement
+
+changeColorsField.forEach((input) =>{
+    input.addEventListener("change", function(): void {
+        fieldDraw.style.background = this.value
+        eraser.value = this.value
+    });
+})
+
+eraser.addEventListener("click", function(): void{
+    color = colorField
+})
+
+const restartGame = document.getElementById("restartGame") as HTMLInputElement
+
+// restartGame.addEventListener("click",function(): void{
+//     fieldDraw.innerHTML = ""
+//     colorField = ""
+//     color = ""
+// })
--- draw/javascript/index.js
+++ draw/javascript/index.js
@@ -2,14 +2,81 @@
 const openSelectColor = document.getElementById("openSelectColor");
 const draw = document.getElementById("draw");
 const selectColor = document.getElementById("selectColor");
+const changeSizeField = document.getElementById("changeSizeField");
+const numColumns = document.getElementById("numColumns");
+const numLines = document.getElementById("numLines");
+const fieldDraw = document.getElementById("fieldDraw");
+let colorField = "#123346";
+let color = "red";
 openSelectColor.addEventListener("click", function () {
     if (draw.classList.value) {
         draw.classList.remove("active");
-        selectColor.classList.remove("active");
+        selectColor.classList.remove("ac tive");
     }
     else {
         draw.classList.add("active");
         selectColor.classList.add("active");
     }
 });
+changeSizeField.addEventListener("click", function () {
+    if (numColumns.value && numLines.value) {
+        fieldDraw.innerHTML = "";
+        const table = document.createElement("table");
+        let isDrawing = false;
+        table.addEventListener("mouseleave", () => {
+            isDrawing = false;
+        });
+        for (let i = 0; i < parseInt(numColumns.value) || 4; i++) {
+            const tr = document.createElement("tr");
+            for (let j = 0; j < parseInt(numLines.value) || 4; j++) {
+                const td = document.createElement("td");
+                td.style.border = "1px solid black";
+                td.addEventListener("mousedown", function () {
+                    isDrawing = true;
+                    td.style.background = color;
+                });
+                td.addEventListener("mousemove", function () {
+                    if (isDrawing)
+                        td.style.background = color;
+                });
+                td.addEventListener("mouseup", function () {
+                    isDrawing = false;
+                });
+                tr.append(td);
+            }
+            table.append(tr);
+        }
+        fieldDraw.append(table);
+    }
+});
+const changeColors = document.querySelectorAll(".changeColors");
+changeColors.forEach((input) => {
+    if (input.type === "button") {
+        input.addEventListener("click", function () {
+            color = this.value;
+        });
+    }
+    else {
+        input.addEventListener("change", function () {
+            color = this.value;
+        });
+    }
+});
+const changeColorsField = document.querySelectorAll(".changeColorsField");
+const eraser = document.getElementById("eraser");
+changeColorsField.forEach((input) => {
+    input.addEventListener("change", function () {
+        fieldDraw.style.background = this.value;
+        eraser.value = this.value;
+    });
+});
+eraser.addEventListener("click", function () {
+    color = colorField;
+});
+const restartGame = document.getElementById("restartGame");
+// restartGame.addEventListener("click",function(): void{
+//     fieldDraw.innerHTML = ""
+//     colorField = ""
+//     color = ""
+// })
 //# sourceMappingURL=index.js.map
