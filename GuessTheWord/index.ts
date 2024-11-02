0dc31bfb2969ae9b305f6e19442b34bf61ae6084
Yaroslav <usern1861@gmail.com>
2024-11-02 23:04:09 +0200

    Reworked 'Guess the Word' project
    - Updated HTML and CSS files
    
    - Started reworking the JavaScript file
    
    - Added configuration for tsconfig.json and .gitignore

---
+++ GuessTheWord/index.ts
@@ -0,0 +1,32 @@
+const blockOne = document.getElementById("blockOne") as HTMLInputElement
+const blockTwo = document.getElementById("blockTwo") as HTMLInputElement
+const blockThree = document.getElementById("blockThree") as HTMLInputElement
+const switchActive = document.getElementById("switchActive") as HTMLSpanElement
+const successfulAnswers = document.getElementById("successfulAnswers") as HTMLSpanElement
+const question = document.getElementById("question") as HTMLParagraphElement
+const hint = document.getElementById("hint") as HTMLParagraphElement
+
+const objQuestion: {[key: string]: string | number} = {
+    "What color is the water?": "blue",
+    "What color is the sun?": "yellow",
+    "What is the name of this game?": "Guess The Word", 
+    "Can you create your own question here?": "Yes"
+};
+
+(function startGame(objQuestion: {[key: string]: string | number}){
+    const keys = Object.keys(objQuestion)
+    const random = Math.ceil(Math.random() * keys.length)
+    question.innerText =  keys[random - 1];
+    hint.innerText = String(objQuestion[keys[random - 1]]);
+    console.log(objQuestion[keys[0]])
+})(objQuestion)
+
+switchActive.addEventListener("click", function(): void{
+    let active = blockOne.classList.toggle("active")
+    blockTwo.classList.toggle("active", active)
+})
+
+successfulAnswers.addEventListener("click", function() {
+    blockThree.classList.toggle("active");
+    successfulAnswers.classList.toggle("active");
+});
