5b2fddb8b9d90317b1089f683434e2f9e4ca1dfb
Yaroslav <usern1861@gmail.com>
2024-11-04 23:45:59 +0200

    Added verification of correctness in 'Guess The Word'

--- GuessTheWord/index.ts
+++ GuessTheWord/index.ts
@@ -4,10 +4,12 @@ const blockThree = document.getElementById("blockThree") as HTMLInputElement
 const switchActive = document.getElementById("switchActive") as HTMLSpanElement
 const successfulAnswers = document.getElementById("successfulAnswers") as HTMLSpanElement
 const question = document.getElementById("question") as HTMLParagraphElement
-const hint = document.getElementById("hint") as HTMLParagraphElement
+const hint = document.getElementById("hint") as HTMLInputElement
 const newQuestion = document.getElementById("newQuestion") as HTMLInputElement
 const newAnswer = document.getElementById("newAnswer") as HTMLInputElement
 const addQuestion = document.getElementById("addQuestion") as HTMLInputElement
+const checkResult = document.getElementById("checkResult") as HTMLInputElement
+const score = document.getElementById("score") as HTMLParagraphElement
 
 const objQuestion: {[key: string]: string | number} = {
     "What color is the water?": "blue",
@@ -16,27 +18,40 @@ const objQuestion: {[key: string]: string | number} = {
     "Can you create your own question here?": "Yes"
 };
 
-(function startGame(objQuestion: {[key: string]: string | number}){
+function startGame(objQuestion: {[key: string]: string | number}){
     const keys = Object.keys(objQuestion)
     const random = Math.ceil(Math.random() * keys.length)
     question.innerText =  keys[random - 1];
-    hint.innerText = String(objQuestion[keys[random - 1]]);
-    console.log(objQuestion[keys[0]])
-})(objQuestion)
+    // hint.innerText = String(objQuestion[keys[random - 1]]);
+}
+
+startGame(objQuestion)
 
 switchActive.addEventListener("click", function(): void{
     let active = blockOne.classList.toggle("active")
     blockTwo.classList.toggle("active", active)
 })
 
-successfulAnswers.addEventListener("click", function() {
+successfulAnswers.addEventListener("click", function(): void{
     blockThree.classList.toggle("active");
     successfulAnswers.classList.toggle("active");
 });
 
-addQuestion.addEventListener("click", function(){
+addQuestion.addEventListener("click", function(): void{
     if(newQuestion.value && newAnswer.value){
         objQuestion[newQuestion.value] = newAnswer.value
-        console.log(objQuestion)
+        // console.log(objQuestion)
+    }
+})
+
+checkResult.addEventListener("click", function (): void {
+    if (question.innerText && hint.value) {
+        if (objQuestion[question.innerText] === hint.value) {
+            delete objQuestion[question.innerText]
+            score.innerText = String(Number(score.innerText) + 1)
+            hint.value = ""
+            startGame(objQuestion)
+            console.log(objQuestion)
+        }
     }
 })
